8afb3bd617aa9801e5ae7bff6976674a6c76bf06
weiok550 <weiok550@gmail.com>
2024-01-06 14:55:36 +0800

    run

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # just for centos
+# 先配置 .env文件
 sudo mkdir -p /usr/local/noss/bin
 sudo cp ./mintnoss /usr/local/noss/bin/
+sudo cp ./mint.sh /usr/local/noss/bin/
+sudo cp .env /usr/local/noss/bin/
 sudo chmod +x /usr/local/noss/bin/mintnoss
+sudo chmod +x /usr/local/noss/bin/mint.sh
 sudo cp ./mintnoss.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable mintnoss
